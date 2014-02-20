ed39380f86caa43beea8348bd1c9e0a7d5cbc278
ruzhuo_feng <ruzhuo_feng@matrixdigi.com>
2014-02-20 21:35:22 +0800

    use blank layout

--- src/Debug/Controller/IndexController.php
+++ src/Debug/Controller/IndexController.php
@@ -14,6 +14,7 @@ class IndexController extends AbstractActionController
         if (!is_file($aConfig['debugconfig']['cachepath'])) {
             echo "debug file '".$aConfig['debugconfig']['cachepath']."' is not exists. ";
         }else
-			echo file_get_contents($aConfig['debugconfig']['cachepath']);		
+			echo file_get_contents($aConfig['debugconfig']['cachepath']);
+        return $this->response;
     }
 }
